983fef514b9b0d5f87d236a34c46e72edbfa126b
Bryce Vandegrift <bryce@brycevandegrift.xyz>
2024-08-03 17:35:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ffsetup
+
+Firefox Setup Script
+
+Setup Firefox automatically with privacy respecting settings.
+This script automatically installs the Arkenfox user.js to firefox.
+
+## Why?
+
+Because I'm tired of manually setting up and configuring Firefox settings
+every time I install a new system. This makes things much easier.
---
+++ ffsetup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Firefox Setup Script
+
+die() {
+	# Print error and exit
+	printf "%s\n" "$1" >&2
+	exit 1
+}
+
+if which firefox >/dev/null 2>&1; then
+	ff=firefox
+elif which firefox-esr >/dev/null 2>&1; then
+	ff=firefox-esr
+else
+	die "Firefox is not installed!"
+fi
+
+# Run Firefox headless to generate a profile
+$ff --headless >/dev/null 2>&1 &
+trap "pkill $ff" HUP INT QUIT TERM PWR EXIT
+
+# Grab profile
+profile="$HOME/.mozilla/firefox/$(grep "Default=.*\.default-release" $HOME/.mozilla/firefox/profiles.ini | sed "s/Default=//")"
+[ ! -d "$profile" ] && die "Could not create/fetch Firefox profile"
+
+# Install Arkenfox user.js
+curl -sL "https://raw.githubusercontent.com/arkenfox/user.js/master/user.js" > "$profile"
